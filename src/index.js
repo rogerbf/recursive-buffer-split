f9e27c2aef57e72878110e3eb49b809a5ed61856
rogerbf <roger@bergfalk.nu>
2016-09-28 19:22:25 +0200

    v0.0.1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-module.exports = {}
+function recursiveSplit (delimiter, buff, results = []) {
+
+  if (
+    delimiter.constructor.name !== `Buffer` ||
+    buff.constructor.name !== `Buffer`
+  ) { throw new TypeError(`Expected buffer`) }
+
+  const delimiterIndex = buff.indexOf(delimiter)
+
+  if (delimiterIndex >= 0) {
+    return recursiveSplit(
+      delimiter,
+      buff.slice(delimiterIndex + delimiter.length),
+      [...results, buff.slice(0, delimiterIndex)]
+    )
+  } else {
+    return [...results, buff]
+  }
+
+}
+
+module.exports = recursiveSplit
