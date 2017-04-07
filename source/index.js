338759045cdea2829f16d576fe39b003d7d83cd4
rogerbf <roger@bergfalk.nu>
2017-04-07 22:23:19 +0200

    progress

--- source/index.js
+++ source/index.js
@@ -1,10 +1,16 @@
 module.exports = function split (separator, source) {
-  const splitter = (source, nextSplit, results = []) => {
-    return (
-      nextSplit === -1
-      ? results.length > 0 ? results : results.concat(source)
-      : undefined
+  const splitter = (
+    source,
+    nextSplit = source.indexOf(separator),
+    results = []
+  ) =>
+    nextSplit < 0
+    ? results.concat(source)
+    : splitter(
+        source.slice(nextSplit + 1),
+        source.slice(nextSplit + 1).indexOf(separator),
+        results.concat(source.slice(0, nextSplit))
     )
-  }
-  return splitter(source, source.indexOf(separator))
+
+  return splitter(source)
 }
